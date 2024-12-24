82e49a8843fd51cc926df5890b0a13e67bf8c44a
alexander193 <sana2001@list.ru>
2024-12-24 17:31:12 +0300

    Test1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(processLine("a1ss    2d"));
+        System.out.println(processLine("a1ss 2d"));
     }
     public static String processLine(String line) {
         StringBuilder reverse = new StringBuilder();
